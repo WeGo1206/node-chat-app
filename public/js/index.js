2b3c64428777937a1849a89f76466b8d872fed4f
WeGo1206 <gweissflog@hotmail.com>
2018-02-11 11:54:31 +0100

    emit broadcast

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,6 @@
 var socket = io();
 socket.on('connect', function () {
   console.log('connected to server');
-
-  socket.emit('createMessage', {
-    from: 'Daisy',
-    text: 'Hello Donald, this is Daisy'
-  });
 });
 
 socket.on('diconnect', function () {
